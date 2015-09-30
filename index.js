71ddf0f0b8364627f0ed027e01dcbbbeb85fa04b
splo1t <splo1t@icloud.com>
2015-09-30 20:54:20 +0300

    normal stats output

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 var SteamApi = require('steam-api');
-var STEAM_API_KEY = 'steam-api-key';
-var optionalSteamId = 'Umaru chan';
-var appId  = 570;
-var user = new SteamApi.User('steam-api-key', optionalSteamId);
-var userStats = new SteamApi.UserStats('steam-api-key', optionalSteamId);
-var news = new SteamApi.News('steam-api-key');
-var app = new SteamApi.App('steam-api-key');
-var player = new SteamApi.Player('steam-api-key', optionalSteamId);
-var inventory = new SteamApi.Inventory('steam-api-key', optionalSteamId);
-var items = new SteamApi.Items('steam-api-key', optionalSteamId);
-app.GetAppList().done(function(result){
-    console.log('no');
-    console.log(result);
-});
-userStats.GetGlobalStatsForGame(appId,optionalSteamId).done(function(result){
-    console.log('no');
-    console.log(result);
+optionalSteamId = '76561198075439630';
+
+var userStats = new SteamApi.UserStats('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
+
+// var user = new SteamApi.User('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
+// var app = new SteamApi.App('FF183132FD171CE0F9853928CDCE1C69');
+// var player = new SteamApi.Player('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
+
+appId = 550;
+
+userStats.GetUserStatsForGame(appId, optionalSteamId).done(function(result){
+  console.log(result);
 });
