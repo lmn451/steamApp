dd0703da72a290226ec9e4c2600617ec1001b22a
lmn451 <cia_agent1@mail.ru>
2015-10-01 16:34:47 +0300

    cleaned up

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var routes = require('./routes/index');
+var http = require('http');
+var path = require('path');
 
-// view engine setup
+app.set('port',3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('express server listening on port '+app.get('port'));
+});
+
+//middleware
+app.use(function(req,res) {
+  if (req.url == '/')
+    res.end("time is money");
+  else
+    res.end("Page Not Found");
+});
+
+/*// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
@@ -31,6 +40,12 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
 
 // error handlers
 
@@ -58,3 +73,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+*/
