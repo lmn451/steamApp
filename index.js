a6295c4befe782319eb66c7f8ca4e8b8d6c7f8bf
splo1t <splo1t@icloud.com>
2015-10-01 03:04:07 +0300

    using parse module instead steam-api module

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var SteamApi = require('steam-api');
-optionalSteamId = '76561198075439630';
+var rest = require('restler');
 
-var userStats = new SteamApi.UserStats('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
+/*
+optionalSteamId = 76561198075439630;
+key = 'FF183132FD171CE0F9853928CDCE1C69';
+*/
 
-// var user = new SteamApi.User('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
-// var app = new SteamApi.App('FF183132FD171CE0F9853928CDCE1C69');
-// var player = new SteamApi.Player('FF183132FD171CE0F9853928CDCE1C69', optionalSteamId);
+/*appId = 570;*/
 
-appId = 550;
-
-userStats.GetUserStatsForGame(appId, optionalSteamId).done(function(result){
-  console.log(result);
-});
+function GetRecentlyPlayedGames(optionalSteamId, key){
+	url = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + key + '&steamid=' + optionalSteamId +'&format=json';
+	rest.get(url).on('complete', function(data) {
+		console.log(data.response.games);
+	});
+}
+GetRecentlyPlayedGames(76561198075439630, FF183132FD171CE0F9853928CDCE1C69);
