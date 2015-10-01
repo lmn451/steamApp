1671e963ea607648891bd5095f71f5e7ad303dae
splo1t <splo1t@icloud.com>
2015-10-01 04:05:41 +0300

    normal steam info output

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var rest = require('restler');
 
-/*
+
 optionalSteamId = 76561198075439630;
 key = 'FF183132FD171CE0F9853928CDCE1C69';
-*/
 
-/*appId = 570;*/
-
-function GetRecentlyPlayedGames(optionalSteamId, key){
+	
+function GetRecentlyPlayedGames(key,optionalSteamId){
 	url = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + key + '&steamid=' + optionalSteamId +'&format=json';
 	rest.get(url).on('complete', function(data) {
-		console.log(data.response.games);
+		for(i=0;i<=data.response.games.length - 1;i++){
+			console.log('Id #' + optionalSteamId + ' played ' + data.response.games[i].name + ' ' + data.response.games[i].playtime_forever / 60 + ' hours at all' );
+		}
+
 	});
 }
-GetRecentlyPlayedGames(76561198075439630, FF183132FD171CE0F9853928CDCE1C69);
+
+GetRecentlyPlayedGames(key,optionalSteamId)
+
